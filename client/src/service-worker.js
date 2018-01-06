52b9eb93fee5096fe44f7b72e65184f13b6ae3bc
Ralph Schaer <ralphschaer@gmail.com>
2018-01-06 12:20:15 +0100

    Upgrade libraries

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -2,6 +2,10 @@ importScripts('workbox-sw.prod.v2.1.2.js');
 
 const workboxSW = new self.WorkboxSW({clientsClaim: true, skipWaiting: true});
 workboxSW.precache([]);
+workboxSW.precache([{
+   "url": "assets/fonts/ionicons.woff2?v=3.0.0-alpha.3"
+ }
+]);
 
 workboxSW.router.registerRoute(/(.*)img(.*)/,
   workboxSW.strategies.cacheFirst({
