4b3a93d0e695dbc4fabc886e2ce1efd75607c473
Ralph Schaer <ralphschaer@gmail.com>
2018-03-23 17:20:20 +0100

    Upgrade to workbox 3

--- client/copy.config.js
+++ client/copy.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   copyWorkbox: {
-    src: ['./node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.1.3.js'],
-    dest: '{{WWW}}'
+	src: [
+	  './node_modules/workbox-sw/build/workbox-sw.js',
+    './node_modules/workbox-core/build/workbox-core.prod.js',
+    './node_modules/workbox-strategies/build/workbox-strategies.prod.js',
+    './node_modules/workbox-cache-expiration/build/workbox-cache-expiration.prod.js',
+    './node_modules/workbox-cacheable-response/build/workbox-cacheable-response.prod.js',
+    './node_modules/workbox-routing/build/workbox-routing.prod.js',
+    './node_modules/workbox-precaching/build/workbox-precaching.prod.js'
+  ],
+	dest: '{{WWW}}/workbox-3.0.0'
   }
 }
--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,19 +1,26 @@
-importScripts('workbox-sw.prod.v2.1.3.js');
+importScripts('workbox-3.0.0/workbox-sw.js');
+workbox.setConfig({
+  debug: false,
+  modulePathPrefix: 'workbox-3.0.0/'
+});
+workbox.skipWaiting();
+workbox.clientsClaim();
+workbox.precaching.precacheAndRoute([]);
+workbox.precaching.precacheAndRoute([{
+  "url": "assets/fonts/ionicons.woff2?v=3.0.0-alpha.3"
+}]);
 
-const workboxSW = new self.WorkboxSW({clientsClaim: true, skipWaiting: true});
-workboxSW.precache([]);
-workboxSW.precache([{
-   "url": "assets/fonts/ionicons.woff2?v=3.0.0-alpha.3"
- }
-]);
-
-workboxSW.router.registerRoute(/(.*)img(.*)/,
-  workboxSW.strategies.cacheFirst({
+workbox.routing.registerRoute(/(.*)img(.*)/,
+  workbox.strategies.cacheFirst({
     cacheName: 'images',
-    cacheExpiration: {
-      maxEntries: 500
-    },
-    cacheableResponse: {statuses: [0, 200]}
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 500
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
   })
 );
 
