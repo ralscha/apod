26022d2a7e7706046c969569678689df2d84dd38
Ralph Schaer <ralphschaer@gmail.com>
2018-06-21 07:07:21 +0200

    Upgrade libraries

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -7,7 +7,7 @@ workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
 workbox.precaching.precacheAndRoute([{
-  "url": "assets/fonts/ionicons.woff2?v=4.1.2"
+  "url": "assets/fonts/ionicons.woff2?v=4.2.0"
 }]);
 
 workbox.routing.registerRoute(/(.*)img(.*)/,
