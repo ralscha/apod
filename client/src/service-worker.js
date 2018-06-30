1dcbe6a061a3f7707e4f6e28d18120c7fc310b3b
Ralph Schaer <ralphschaer@gmail.com>
2018-06-30 06:19:23 +0200

    Uprade ionicons

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -7,7 +7,7 @@ workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
 workbox.precaching.precacheAndRoute([{
-  "url": "assets/fonts/ionicons.woff2?v=4.2.1"
+  "url": "assets/fonts/ionicons.woff2?v=4.2.3"
 }]);
 
 workbox.routing.registerRoute(/(.*)img(.*)/,
