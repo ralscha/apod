75c3f389dde13cae8778ff7c3baa713463bed1d6
Ralph Schaer <ralphschaer@gmail.com>
2018-07-24 07:29:11 +0200

    Upgrade ionicons

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -7,7 +7,7 @@ workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
 workbox.precaching.precacheAndRoute([{
-  "url": "assets/fonts/ionicons.woff2?v=4.2.3"
+  "url": "assets/fonts/ionicons.woff2?v=4.2.4"
 }]);
 
 workbox.routing.registerRoute(/(.*)img(.*)/,
