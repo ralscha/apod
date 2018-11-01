db1177fe426861954773a75167a827e186f73fcd
Ralph Schaer <ralphschaer@gmail.com>
2018-11-01 08:22:32 +0100

    Upgrade libraries

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,13 +1,13 @@
-importScripts('workbox-3.6.2/workbox-sw.js');
+importScripts('workbox-3.6.3/workbox-sw.js');
 workbox.setConfig({
   debug: false,
-  modulePathPrefix: 'workbox-3.6.2/'
+  modulePathPrefix: 'workbox-3.6.3/'
 });
 workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
 workbox.precaching.precacheAndRoute([{
-  "url": "assets/fonts/ionicons.woff2?v=4.4.3"
+  "url": "assets/fonts/ionicons.woff2?v=4.4.5"
 }]);
 
 workbox.routing.registerRoute(/(.*)img(.*)/,
