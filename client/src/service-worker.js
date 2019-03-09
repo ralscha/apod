b3a5d4c25070a9b8df46435ed447699d60983f92
Ralph Schaer <ralphschaer@gmail.com>
2019-03-09 12:52:13 +0100

    Upgrade libraries

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,7 +1,7 @@
-importScripts('workbox-4.0.0/workbox-sw.js');
+importScripts('workbox-4.1.0/workbox-sw.js');
 workbox.setConfig({
   debug: false,
-  modulePathPrefix: 'workbox-4.0.0/'
+  modulePathPrefix: 'workbox-4.1.0/'
 });
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
