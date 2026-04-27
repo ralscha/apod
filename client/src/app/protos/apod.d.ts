69e03de5e6303ab4452e26c5966c030cf9fc9d4b
Ralph Schaer <ralphschaer@gmail.com>
2026-04-27 20:09:23 +0200

    upgrade

--- client/src/app/protos/apod.d.ts
+++ client/src/app/protos/apod.d.ts
@@ -1,73 +1,45 @@
-import * as $protobuf from 'protobufjs';
-
+import * as $protobuf from "protobufjs";
+import Long = require("long");
 export interface IApod {
-  date?: string | null;
-  explanation?: string | null;
-  title?: string | null;
-  credit?: string | null;
+    date?: (string|null);
+    explanation?: (string|null);
+    title?: (string|null);
+    credit?: (string|null);
 }
 
 export class Apod implements IApod {
-  public date: string;
-  public explanation: string;
-  public title: string;
-  public credit: string;
-
-  constructor(properties?: IApod);
-
-  public static create(properties?: IApod): Apod;
-
-  public static encode(message: IApod, writer?: $protobuf.Writer): $protobuf.Writer;
-
-  public static encodeDelimited(message: IApod, writer?: $protobuf.Writer): $protobuf.Writer;
-
-  public static decode(reader: $protobuf.Reader | Uint8Array, length?: number): Apod;
-
-  public static decodeDelimited(reader: $protobuf.Reader | Uint8Array): Apod;
-
-  public static verify(message: Record<string, unknown>): string | null;
-
-  public static fromObject(object: Record<string, unknown>): Apod;
-
-  public static toObject(
-    message: Apod,
-    options?: $protobuf.IConversionOptions,
-  ): Record<string, unknown>;
-
-  public static getTypeUrl(typeUrlPrefix?: string): string;
-
-  public toJSON(): Record<string, unknown>;
+    constructor(properties?: IApod);
+    public date: string;
+    public explanation: string;
+    public title: string;
+    public credit: string;
+    public static create(properties?: IApod): Apod;
+    public static encode(message: IApod, writer?: $protobuf.Writer): $protobuf.Writer;
+    public static encodeDelimited(message: IApod, writer?: $protobuf.Writer): $protobuf.Writer;
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): Apod;
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): Apod;
+    public static verify(message: { [k: string]: any }): (string|null);
+    public static fromObject(object: { [k: string]: any }): Apod;
+    public static toObject(message: Apod, options?: $protobuf.IConversionOptions): { [k: string]: any };
+    public toJSON(): { [k: string]: any };
+    public static getTypeUrl(typeUrlPrefix?: string): string;
 }
 
 export interface IApods {
-  apods?: IApod[] | null;
+    apods?: (IApod[]|null);
 }
 
 export class Apods implements IApods {
-  public apods: IApod[];
-
-  constructor(properties?: IApods);
-
-  public static create(properties?: IApods): Apods;
-
-  public static encode(message: IApods, writer?: $protobuf.Writer): $protobuf.Writer;
-
-  public static encodeDelimited(message: IApods, writer?: $protobuf.Writer): $protobuf.Writer;
-
-  public static decode(reader: $protobuf.Reader | Uint8Array, length?: number): Apods;
-
-  public static decodeDelimited(reader: $protobuf.Reader | Uint8Array): Apods;
-
-  public static verify(message: Record<string, unknown>): string | null;
-
-  public static fromObject(object: Record<string, unknown>): Apods;
-
-  public static toObject(
-    message: Apods,
-    options?: $protobuf.IConversionOptions,
-  ): Record<string, unknown>;
-
-  public static getTypeUrl(typeUrlPrefix?: string): string;
-
-  public toJSON(): Record<string, unknown>;
+    constructor(properties?: IApods);
+    public apods: IApod[];
+    public static create(properties?: IApods): Apods;
+    public static encode(message: IApods, writer?: $protobuf.Writer): $protobuf.Writer;
+    public static encodeDelimited(message: IApods, writer?: $protobuf.Writer): $protobuf.Writer;
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): Apods;
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): Apods;
+    public static verify(message: { [k: string]: any }): (string|null);
+    public static fromObject(object: { [k: string]: any }): Apods;
+    public static toObject(message: Apods, options?: $protobuf.IConversionOptions): { [k: string]: any };
+    public toJSON(): { [k: string]: any };
+    public static getTypeUrl(typeUrlPrefix?: string): string;
 }
